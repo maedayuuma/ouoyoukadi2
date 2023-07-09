cfd03acb8875df70f724a8f80c99212916d6d1f5
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-07-09 13:10:53 +0000

    応用課題6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,22 +1,19 @@
 class PostCommentsController < ApplicationController
-
   def create
-
-      @book = Book.find(params[:book_id])
-      @user = @book.user
-      comment = current_user.post_comments.new(post_comment_params)
-      comment.book_id = @book.id
-   if comment.save
-      redirect_to book_path(@book.id)
-   else
-      @post_comment = PostComment.new
-      render template: "books/show"
-   end
+    @book = Book.find(params[:book_id])
+    @user = @book.user
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.book_id = @book.id
+    @post_comment.save
+    #redirect_to book_path(@book)
   end
 
+
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @post_comment = PostComment.find(params[:id]).destroy
+    @post_comment.destroy
+    @book = Book.find(params[:book_id])
+    #redirect_to request.referer
   end
 
   private
